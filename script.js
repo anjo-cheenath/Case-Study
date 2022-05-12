e1230bf1edccb7e8a3aefbb412801625fac6c6ae
anjo-cheenath <cheenathjoseph@gmail.com>
2022-05-12 11:24:13 +0530

    submission1

---
+++ script.js
@@ -0,0 +1,100 @@
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+
+function validate(username,password,callback){
+    let err = false;
+    if( username.value == "admin" && password.value == 12345){
+        err = true;
+    }
+    callback(err);
+
+    
+    
+}
+
+function redirect(err){
+    if(err){
+        window.open("./main.html");
+    }
+    else{
+        alert("error");
+    }
+}
+
+function displaylist(){
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange=function(){
+        try{
+            if(httpRequest.readyState===XMLHttpRequest.DONE){
+                if(httpRequest.status ===200){
+                    console.log(httpRequest.responseText);
+                    display(httpRequest.responseText)
+                }
+                else{
+                    alert("Error from API");
+                }
+            }
+        }
+        catch(e){
+            alert(e.description);
+        }
+    };
+    try{
+        httpRequest.open('GET', 'https://jsonplaceholder.typicode.com/todos',true);
+        httpRequest.send();
+    }
+    catch(e){
+        alert(e.description);
+    }
+}
+
+function logout(){
+    window.location.href="login.html";
+}
+
+function display(data){
+    var list = JSON.parse(data);
+    let table = document.getElementById("todotable");
+    
+    for(var i=0;i<list.length;i++){
+        let rowcount = table.rows.length;
+        var row = table.insertRow(rowcount);
+        var cell1 = row.insertCell(0);
+        cell1.innerHTML=list[i].id;
+
+        var cell2 = row.insertCell(1);
+        cell2.innerHTML=list[i].title;
+
+        var cell3 = row.insertCell(2);
+        var element= document.createElement("input");
+        element.type="checkbox";
+
+        if(list[i].completed==true){
+            element.setAttribute("checked","true");
+            element.setAttribute("disabled","true");
+        }
+        element.addEventListener('change',(event)=>{
+            if(event.currentTarget.checked){
+                count++;
+                checkCounter();
+            }
+            else{
+                count--;
+            }
+        })
+        cell3.appendChild(element);
+    }
+}
+
+var count = 0;
+function checkCounter(){
+    let promise = new Promise(function(resolve,reject){
+        if(count==5){
+            resolve("Congrats. 5 Tasks have been Successfully Completed");
+        }
+
+    })
+    promise.then(function(msg){
+        alert(msg);
+    })
+}
